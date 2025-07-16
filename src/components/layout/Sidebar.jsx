348bb2368c73a5560ee9e762a0b558f7987b51d6
Fernando <araujofernando592@gmail.com>
2025-07-16 15:35:37 -0300

    Sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,17 +1,17 @@
+import Button from "../ui/Button.jsx";
 function Sidebar(){
     return(
-        <aside className="h-screen">
-            <nav className="h-full flex flex-col bg-zinc-0 border-r shadow-sm">
-                <div className="p-4 pb-2 flex justify-between items-center">
-                    <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNhoXisDruJMDAq3Ltd-wuaMW2lGxck9wAKw&s"
-                        className="w-32"
-                        alt=""
-                    />
-                    
-                </div>
-            </nav>
-        </aside>
+        <nav className="shadow-md h-screen w-60 bg-blue-700">
+            <div className="p-4 pb-2 flex justify-between items-center">
+                <img
+                    src="../../assets/"
+                    className="w-32"
+                    alt=""
+                />
+                <Button text="voltar"/>
+            </div>
+        </nav>
+    
     );
 }
 
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,6 +1,6 @@
 function button ({text, onClick}){
   return(
-    <button className="btn-true" onClick={onClick}>
+    <button className="btn-true p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100" onClick={onClick}>
       {text}
     </button>
   )
