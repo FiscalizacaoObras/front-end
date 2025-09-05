b60f304747cd6f8a854c152616b6ab21f476460a
LFelipedev <luiz.fma.up@gmail.com>
2025-09-05 11:40:54 -0300

    fix corrigindo bug que não carrega a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import Button from "../components/ui/Button";
 import Sidebar from "../components/layout/Sidebar";
 
+function Home() {
+    return (
+        <>
+            <div className="flex">
+                <Sidebar />
+                <div className="p-4 flex flex-col gap-4">
+                    <h1 className="text-xl font-bold">Home</h1>
 
-function Home(){
-    <>
-        <div className="flex">
-            <Sidebar/>
-            <div className="p-4 flex flex-col gap-4">
-               <h1 className="text-xl font-bold">Home</h1>
-
-                {/* Botões visíveis */}
-                <Button variant="secondary" text="Voltar" />
-                <Button text="Avançar" />
+                    {/* Botões visíveis */}
+                    <Button variant="secondary" text="Voltar" />
+                    <Button text="Avançar" />
+                </div>
             </div>
-        </div>
-    </>
+        </>
+    )
 }
 export default Home;
