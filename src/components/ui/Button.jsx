887462e92233ec19fac5e3a33d7c209fdafff44f
LFelipedev <luiz.fma.up@gmail.com>
2025-09-22 10:42:24 -0300

    spaces

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,22 +1,22 @@
 function Button({ icon, text, variant = 'primary', onClick }) {
-  let variantClass = "";
+    let variantClass = "";
 
-  if (variant === "primary") {
-    variantClass = "bg-[var(--vinho-primario)] text-white border border-[var(--vinho-primario)]";
-  } 
-  else {
-    variantClass = "bg-white text-[var(--vinho-primario)] border border-[var(--vinho-primario)]";
-  }
+    if (variant === "primary") {
+        variantClass = "bg-[var(--vinho-primario)] text-white border border-[var(--vinho-primario)]";
+    }
+    else {
+        variantClass = "bg-white text-[var(--vinho-primario)] border border-[var(--vinho-primario)]";
+    }
 
-  return (
-    <button
-      className={`flex items-center gap-0.5 p-2 rounded-lg cursor-pointer ${variantClass}`}
-      onClick={onClick}
-    >
-      {icon && <span>{icon}</span>}
-      <span>{text}</span>
-    </button>
-  );
+    return (
+        <button
+            className={`flex items-center gap-0.5 p-2 rounded-lg cursor-pointer ${variantClass}`}
+            onClick={onClick}
+        >
+            {icon && <span>{icon}</span>}
+            <span>{text}</span>
+        </button>
+    );
 }
 
 export default Button;
