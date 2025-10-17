ee1e80874e5d8465de03b8003645870df435c9e0
FernandoVini <araujofernando592@gmail.com>
2025-10-17 14:43:03 -0300

    create diferent types of uploads in  upload templates

--- src/components/ui/Upload.jsx
+++ src/components/ui/Upload.jsx
@@ -5,6 +5,7 @@ import { message, Input, Tooltip, Checkbox, Button } from 'antd';
 
 function Upload() {
     const [pdfList, setPdfList] = useState([]);
+    const [UploadBig, setUploadBig] = useState(true);
     const [nomeSelecao, setNomeSelecao] = useState(() => {
         return localStorage.getItem('nomeSelecao') || '';
     });
@@ -43,33 +44,65 @@ function Upload() {
         setPdfList(updatedList);
     };
 
-    return (
-        <div className="flex flex-col gap-5 p-10 w-[90%] h-full pb-[15%] bg-white mx-auto shadow-md rounded-lg">
-            <div className="flex flex-col items-start gap-2.5">
-                <h2 className="text-2xl font-semibold text-gray-800 text-center">Upload Arquivo</h2>
-                <p>Lorem ipsum.... Descrição do que deve ser esse upload</p>
-            </div>
-            <label className="border-2 border-dashed border-gray-300 p-10 text-center block cursor-pointer rounded-xl mb-10">
-                <div className="flex flex-col items-center">
-                    <div>
-                        <FiUploadCloud className="text-[32px] text-gray-700" />
-                    </div>
+    useEffect(() => {
+        if (pdfList.length > 0) {
+            setUploadBig(false);
+        } else {
+            setUploadBig(true);
+        }
+    }, [pdfList]);
 
-                    <p className="mt-2">
-                        <span className="text-indigo-800 font-bold">Clique em Upload</span> ou arraste e solte um arquivo
-                    </p>
-                    <p>PNG, PDF, JPG ou DOCX</p>
-                    <input
-                        id="pdf-upload"
-                        type="file"
-                        accept="application/pdf"
-                        onChange={handleFileChange}
-                        className="hidden"
-                        multiple
-                    />
+    return (
+        <>
+            {UploadBig && (<div className="flex flex-col bg-white mx-auto shadow-md rounded-lg gap-5 p-10 w-[90%] h-full pb-[15%]">
+                <div className="flex flex-col items-start gap-2.5">
+                    <h2 className="text-2xl font-semibold text-gray-800 text-center">Upload Arquivo</h2>
+                    <p>Lorem ipsum.... Descrição do que deve ser esse upload</p>
                 </div>
-            </label>
-        </div>
+                <label className="border-2 border-dashed border-gray-300 p-10 text-center block cursor-pointer rounded-xl mb-10">
+                    <div className="flex flex-col items-center">
+                        <div>
+                            <FiUploadCloud className="text-[32px] text-gray-700" />
+                        </div>
+
+                        <p className="mt-2">
+                            <span className="text-indigo-800 font-bold">Clique em Upload</span> ou arraste e solte um arquivo
+                        </p>
+                        <p>PNG, PDF, JPG ou DOCX</p>
+                        <input
+                            id="pdf-upload"
+                            type="file"
+                            accept="application/pdf"
+                            onChange={handleFileChange}
+                            className="hidden"
+                            multiple
+                        />
+                    </div>
+                </label>
+            </div>)}
+            {!UploadBig && (
+                <>
+                    <div className="flex flex-col bg-white mx-auto shadow-md rounded-lg gap-5 p-10 w-[90%] h-10 pb-[15%]">
+                        <label className="border-2 border-dashed border-gray-300 p-5 text-center block cursor-pointer rounded-xl mb-10">
+                            <div className="flex flex-col items-center">
+                                <p className="mt-2">
+                                    <span className="text-indigo-800 font-bold">Clique em Upload</span> ou arraste e solte um arquivo
+                                </p>
+                                <p>PNG, PDF, JPG ou DOCX</p>
+                                <input
+                                    id="pdf-upload"
+                                    type="file"
+                                    accept="application/pdf"
+                                    onChange={handleFileChange}
+                                    className="hidden"
+                                    multiple
+                                />
+                            </div>
+                        </label>
+                    </div>
+                </>
+            )}
+        </>
     );
 }
 
