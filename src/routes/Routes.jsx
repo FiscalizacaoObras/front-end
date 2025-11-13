3b910c37c5f15ad3a12154086101f836af94d3f4
FernandoVini <araujofernando592@gmail.com>
2025-11-13 18:50:48 -0300

    update routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import ViewTemplates from "../pages/ViewTemplates.jsx";
 import Create from "../pages/Create.jsx";
+import UploadDocument from "../pages/UploadDocument.jsx";
 
 function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ function AppRoutes() {
       <Route index element={<ViewTemplates />} />
       <Route path="/visualizar" element={<ViewTemplates />} />
       <Route path="/criar" element={<Create />} />
+      <Route path="/enviar/:id" element={<UploadDocument/>}/>
     </Routes>
   );
 }
