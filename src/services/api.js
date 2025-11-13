fb1224d4aa52da005519ec4250b6ae6993c71108
FernandoVini <araujofernando592@gmail.com>
2025-11-13 20:34:53 -0300

    update api grok

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url =  'https://bbd79f93a96c.ngrok-free.app ';
+const url ='https://bbd79f93a96c.ngrok-free.app';
 
 export async function getTemplate() {
     const response = await axios.get(`${url}/templates`);
